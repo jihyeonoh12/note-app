3bcf90759082cce8b02907086accbdbc1f5dcebe
jihyeonoh12 <jihyeonoh12@gmail.com>
2023-01-27 14:03:11 -0800

    Updates

---
+++ precache-manifest.887b20f868751b9a2b675d8237e23f47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c6e61e88a90ebc486d4",
+    "url": "/note-app/static/css/main.67e8098c.chunk.css"
+  },
+  {
+    "revision": "2c6e61e88a90ebc486d4",
+    "url": "/note-app/static/js/main.77a3351a.chunk.js"
+  },
+  {
+    "revision": "d89c131aa0fba350078c",
+    "url": "/note-app/static/js/runtime~main.35a9a3bf.js"
+  },
+  {
+    "revision": "25bc4880b6f4800ae9bf",
+    "url": "/note-app/static/css/2.a41f7588.chunk.css"
+  },
+  {
+    "revision": "25bc4880b6f4800ae9bf",
+    "url": "/note-app/static/js/2.d93b9874.chunk.js"
+  },
+  {
+    "revision": "9417166b9fd15ad1ab18",
+    "url": "/note-app/static/js/3.69c2b8f0.chunk.js"
+  },
+  {
+    "revision": "aef2caccbe1babf31cad09235c0626a2",
+    "url": "/note-app/index.html"
+  }
+];
